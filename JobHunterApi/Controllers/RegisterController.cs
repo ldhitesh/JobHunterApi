51546877d1145021088a89fdd520ab13f063060d
ldhitesh <ldhitesh.02@gmail.com>
2024-11-25 21:43:31 -0600

    ...

--- JobHunterApi/Controllers/RegisterController.cs
+++ JobHunterApi/Controllers/RegisterController.cs
@@ -1,15 +1,37 @@
+using JobHunterApi.Database;
 using JobHunterApi.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 [Route("api/[controller]")]
 [ApiController]
 public class RegisterController : ControllerBase
 {
     private readonly UserManager<IdentityUser> _userManager;
-    public RegisterController(UserManager<IdentityUser> userManager)
+
+    private readonly CompaniesDbContext _context;
+
+    public RegisterController(UserManager<IdentityUser> userManager,
+                            CompaniesDbContext context)
     {
         _userManager = userManager;
+        _context= context;
+    }
+    [HttpGet("getpendingregistrations")]
+    public async Task<IActionResult> GetPendingApplications()
+    {
+        var pendingregistrations = await _context.PendingRegistrations.ToListAsync(); 
+        return Ok(pendingregistrations);
+    }
+
+    [HttpPost("pendingregistrations")]
+    public async Task<IActionResult> GetPendingApplications([FromBody] RegisterModel user)
+    {
+        await _context.PendingRegistrations.AddAsync(user);
+        await _context.SaveChangesAsync();
+
+        return Ok(new {Message="user approval pending"});
     }
 
     [HttpPost]
@@ -29,12 +51,18 @@ public class RegisterController : ControllerBase
             Email = model.Email
         };
 
+        if (!await _userManager.IsInRoleAsync(user, "admin") && user.UserName=="Hitesh")
+        {
+            await _userManager.AddToRoleAsync(user, "admin");
+        }
+
         var result = await _userManager.CreateAsync(user, model.Password);
 
         if (!result.Succeeded)
         {
             return BadRequest(result.Errors);
         }
+        
 
         return Ok(new { Message="Registered Successfully! Please login!" });
     }
