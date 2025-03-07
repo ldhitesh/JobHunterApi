f9aced248ac71bc492be5f3998e607143a7ba781
ldhitesh <ldhitesh.02@gmail.com>
2025-03-07 05:17:07 -0600

    confirm password added to register model and pending registrations datbase

--- JobHunterApi/Models/RegisterModel.cs
+++ JobHunterApi/Models/RegisterModel.cs
@@ -6,6 +6,7 @@ namespace JobHunterApi.Models
         public string Username { get; set; }=string.Empty;
         public string Email { get; set; } =string.Empty;
         public string Password { get; set; }=string.Empty;
+        public string ConfirmPassword { get; set; }=string.Empty;
         public string PaymentStatus { get; set; }=string.Empty;
         public string VerificationToken { get; set; }=string.Empty;
         public string AccountVerificationStatus { get; set; }="Not Verified";
